7b04e023f8677bba43b31dd9b5346f90575cd82c
gilgulart <gilbertogulart70@gmail.com>
2024-04-02 04:08:11 -0300

    exercitando a lógica

--- exercícios/ex03/index.php
+++ exercícios/ex03/index.php
@@ -4,42 +4,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Aula</title>
+
     <link rel="shortcut icon" href="image/masqueico.ico" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+
     <h1>Aula e Presença</h1>
     <h2>Aula normal: o professor e os alunos estavam presentes</h2>
+
     <?php 
     require_once "lesson.php" ;
-    //aula normal
     $lesson = new lesson ;
     $lesson -> teacher = true ;
     $lesson -> student = true ;
     $lesson -> teacherPresent() ;
     $lesson -> studentPresent() ;
-
     ?>
-    <h2>O professor precisou descansar</h2>
+
+    <h2>O professor precis ou descansar</h2>
+
     <?php 
-  $noLesson = new lesson ;
-  $noLesson -> teacher = false ;
-  $noLesson -> teacherPresent() ;
- ?>
+    $noLesson = new lesson ;
+    $noLesson -> teacher = false ;
+    $noLesson -> teacherPresent() ;
+    ?>
     <h2>O aluno faltou</h2>
     <?php 
- $ausent = new lesson ;
- $ausent -> student = false ;
- $ausent -> studentPresent() ;
- ?>
+    $ausent = new lesson ;
+    $ausent -> student = false ;
+    $ausent -> studentPresent() ;
+    ?>
     <h2>Os alunos mataram aula</h2>
     <?php 
- $skipLesson = new lesson ;
- $skipLesson -> student < 1 ;
- $skipLesson -> studentSkip() ;
- ?>
+    $skipLesson = new lesson ;
+    $skipLesson -> student < 1 ;
+    $skipLesson -> studentSkip() ;
+    ?>
 </body>
 
 </html>
