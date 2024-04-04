26bfdce04d3bf0195692e93327d082e5af177cc1
gilgulart <gilbertogulart70@gmail.com>
2024-04-04 01:17:46 -0300

    banco virtual

--- desafios/desafio1/classBank.php
+++ desafios/desafio1/classBank.php
@@ -1,48 +1,65 @@
 <?php 
 class Account{
-public $numberAccount ;
-protected $type ;
-private $owner ;
-private $balance ;
-private $status ;
+     public $numberAccount ;
+     protected $type ;
+     private $owner ;
+     private $balance ;
+     private $status ;
 
 //criando objeto comum: conta
-public function __construct($numberAccount,$type, $owner,$balance,$status){
-    $this -> numberAccount = $numberAccount ;
-    $this -> type = $type ;
-    $this -> owner = $owner ;
-    $this -> balance = $balance ;
-    $this -> status = $status ;
+     public function __construct($numberAccount,$type, $owner,$balance,$status){
+         $this -> numberAccount = $numberAccount ;
+         $this -> type = $type ;
+         $this -> owner = $owner ;
+         $this -> balance = $balance ;
+         $this -> status = $status ;
 
-}
+     }
 
-public function getType(){
-    return $this -> type ;
-}
-public function getOwner(){
-    return $this -> owner ;
-}
-public function getBalance(){
-    return $this -> balance ;
-}
-public function getStatus(){
-    return $this -> status ;
-}
-public function setBalance($newBalance){
-    $this-> balance = $newBalance;
-}
-
-
-
-public function openAccount(){
-    if ($this-> type === "CC") {
-        $this -> balance += 50 ;
-        echo "Sua Conta Corrente rendeu R\$50 como incentivo\n" ;
-    } else {
-        $this -> balance += 150 ;
-        echo "Sua Conta Poupança rendeu R\$150 como incentivo" ;
-    }
-}
+     public function getType(){
+         return $this -> type ;
+     }
+     public function getOwner(){
+         return $this -> owner ;
+     }
+     public function getBalance(){
+         return $this -> balance ;
+     }
+     public function getStatus(){
+         return $this -> status ;
+     }
+     public function setBalance($newBalance){
+         $this-> balance = $newBalance;
+     }
 
 
+
+     public function activateAccount(){
+        
+         
+         if ($this-> type === "Corrente") {
+             $this -> balance += 50 ;
+             echo "Sua Conta Corrente rendeu R\$50 como incentivo\n" ;
+            } else {
+                $this -> balance += 150 ;
+                echo "Sua Conta Poupança rendeu R\$150 como incentivo" ;
+            }
+            $this->status = true ;
+        }
+     public function closeAccount(){
+         $this->status = false ;
+        
+     }
+     public function deposit($amount) {
+        if ($amount > 0) {
+            if ($this->type === "Corrente"){
+                $this->balance += $amount + 50 ;
+            }else {
+                $this->balance += $amount + 150 ;
+            }
+            echo "Depósito de R\$ {$amount} realizado com sucesso. Seu novo saldo é: R\$ {$this->balance}\n";
+        } else {
+            echo "Valor de depósito inválido.\n";
+        }
+     }
 }
--- desafios/desafio1/index.php
+++ desafios/desafio1/index.php
@@ -11,9 +11,10 @@
 <body>
     <main>
         <h1> Gil Bank </h1>
-        <form action="<?=$_SERVER ['PHP_SELF'] ?>" method="get">
+        <form action="<?=$_SERVER ['PHP_SELF'] ?>" method="post">
             <input type="text" name="owner" id="owner" placeholder="Usuário">
-            <input type="number" name="numberAccount" id="numberAccount" placeholder="Número da conta" value="">
+            <input type="number" name="numberAccount" id="numberAccount" placeholder="Número da conta"
+                value="<?=$numberAccount?>">
             <button type="submit">Enviar</button>
         </form>
     </main>
@@ -39,17 +40,43 @@
         </div>
         <?php 
     require_once "classBank.php" ;
-    $jubileo = new Account(1234,"CC","Jubileo",5000,true) ;
-    $creuza = new Account(4321,"CP","Creuza",1200,false) ;
-    if (isset($_GET['owner']) && isset ($_GET['numberAccount'])) {
-
-        $numberAcount = $_GET['numberAccount'] ;
-        $owner = $_GET['owner'] ;
+         $jubileo = new Account(1234,"Corrente","Jubileo",5000,true) ;
+         $creuza = new Account(4321,"Poupança","Creuza",1200,false) ;
+         function renderDepositForm($accountType) {
+            echo "<form action='{$_SERVER['PHP_SELF']}' method='post'>" ;
+            echo "<input type='hidden' name='accountType' value='$accountType'>" ;
+            echo "<input type='number' name='depositAmount' placeholder='Valor a depositar'>";
+            echo "<button type='submit'>Depositar</button>";
+            echo "</form>";
+        }
     
-    if ($owner ==='')    
-        
-    }
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['owner']) && isset($_POST['numberAccount'])) {
+            $numberAccount = $_POST['numberAccount'];
+            $owner = $_POST['owner'];
+            if ($owner === "Jubileo" && $numberAccount == $jubileo->numberAccount) {
+                $jubileo->activateAccount();
+                echo "<p>Senhor Jubileo, sua conta {$jubileo->getType()} número {$jubileo->numberAccount} tem saldo: R$ {$jubileo->getBalance()}</p>";
+                renderDepositForm("Jubileo");
+             } elseif ($owner === "Creuza" && $numberAccount == $creuza->numberAccount) {
+                $creuza->activateAccount();
+                echo "<p>Senhora Creuza, sua Conta {$creuza->getType()} número {$creuza->numberAccount} tem saldo: R$ {$creuza->getBalance()}</p>";
+                renderDepositForm("Creuza");
+             } else {
+                echo "<p>Usuário ou número de conta inválido</p>";
+             }
+        }
+    
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['depositAmount'])) {
+            $selectedAccount = $_POST['accountType'];
+            $depositAmount = $_POST['depositAmount'];
+            if ($selectedAccount === "Jubileo") {
+                $jubileo->deposit($depositAmount);
+             } else{
+                $creuza->deposit($depositAmount);
+             }
+        }
     ?>
+
     </section>
 </body>
 
